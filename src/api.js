4e67e4a42486aaa46dde80f9574a6717fe59af98
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-08-26 18:16:50 -0300

    feat: instalação do módulo externo 'express-async-errors' e importação desse no 'src/api.js'

--- src/api.js
+++ src/api.js
@@ -1,10 +1,17 @@
 const express = require('express');
-
+require('express-async-errors');
+const login = require('./controllers/loginController');
 // ...
 
 const app = express();
 
 app.use(express.json());
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const { code, message } = await login.login({ email, password });
+  return res.status(code).json(message);
+});
 
 // ...
 
