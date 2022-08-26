adeb960166c3a546bb2c237fd659a80bf0f44150
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-08-25 23:52:32 -0300

    feat: Req. 07 - criado model Category em models/category.js com propriedades básicas da tabela Categories

---
+++ src/database/models/category.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING
+    },    
+  })
+  return Category;
+};
